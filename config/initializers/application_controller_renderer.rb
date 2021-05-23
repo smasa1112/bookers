70b622baa48d46aeb64055d7bd6f981010b5b68c
smasa1112 <mtsakagami1112@yahoo.co.jp>
2021-05-23 15:10:37 +0000

    [complete]booker2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0929fde2fa420cc8603e82a017ce1b6552e6d6e121331d594a821a5bfd273e3ed89ecc1d0f4b3acd80e5615cd5509d67b84dd03cc1db06b3f657fb415dbf0db3'
