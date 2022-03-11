51786835ec62bea181b2cb93131347aab2eca1b7
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-03-11 17:58:38 +0000

    [fix]migrationファイルの本番環境エラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -117,7 +117,6 @@ ActiveRecord::Schema.define(version: 2022_03_11_081314) do
     t.datetime "created_at"
     t.string "tenant", limit: 128
     t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
     t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
     t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
@@ -133,7 +132,6 @@ ActiveRecord::Schema.define(version: 2022_03_11_081314) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
